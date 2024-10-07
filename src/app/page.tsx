d02e913275332350975506c37557950f6d619e8f
VTEXsx <8221vitor@gmail.com>
2024-10-07 10:45:20 -0300

    adicionado o header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@/components/Header";
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-blue-3 p-24">
-      <div>
-        <h1 className="text-H1 text-blue-1">Repositorio Phoenix coders</h1>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-blue-3">
+      <Header />
+      <h1 className="text-H1 text-blue-1">Repositorio Phoenix coders</h1>
     </main>
   );
 }
